630470e95c39626a508134f93319d7bc33e55e1c
Starblo <956754852@qq.com>
2018-05-17 13:38:08 +0800

    test

---
+++ Compiler/Compiler/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Compiler
+//
+//  Created by Starblo Hong on 17/05/2018.
+//  Copyright © 2018 Starblo Hong. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
