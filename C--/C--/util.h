4f222ee89c075f3e950480fb4be38dfefe06fc67
Starblo <956754852@qq.com>
2018-05-19 21:56:27 +0800

    omg

--- C--/C--/util.h
+++ C--/C--/util.h
@@ -6,18 +6,9 @@
 //  Copyright © 2018 Starblo Hong. All rights reserved.
 //
 
-#include <assert.h>
+#ifndef util_h
+#define util_h
 
-typedef char *string;
-typedef char bool;
+#include <stdio.h>
 
-#define TRUE 1
-#define FALSE 0
-
-void *checked_malloc(int);
-string String(char *);
-
-typedef struct U_boolList_ *U_boolList;
-struct U_boolList_ {bool head; U_boolList tail;};
-U_boolList U_BoolList(bool head, U_boolList tail);
-
+#endif /* util_h */
