7e9049691776c2970aab7792503cd700492cb9d8
Starblo <956754852@qq.com>
2018-05-19 21:58:55 +0800

    lexical

--- C--/C--/util.h
+++ C--/C--/util.h
@@ -6,9 +6,18 @@
 //  Copyright © 2018 Starblo Hong. All rights reserved.
 //
 
-#ifndef util_h
-#define util_h
+#include <assert.h>
 
-#include <stdio.h>
+typedef char *string;
+typedef char bool;
 
-#endif /* util_h */
+#define TRUE 1
+#define FALSE 0
+
+void *checked_malloc(int);
+string String(char *);
+
+typedef struct U_boolList_ *U_boolList;
+struct U_boolList_ {bool head; U_boolList tail;};
+U_boolList U_BoolList(bool head, U_boolList tail);
+
