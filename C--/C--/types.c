1ee6a032331fad75f460619501f97eee2835a6d5
dsmnfbnfb <406161167@qq.com>
2018-05-31 02:51:37 +0800

    加类型

--- C--/C--/types.c
+++ C--/C--/types.c
@@ -1,33 +1,24 @@
-/*
-* types.c -
-*
-* All types and functions declared in this header file begin with "Ty_"
-* Linked list types end with "..list"
-*/
-
+//
+//
+//by LBQ
+//Reference: Textbook Page 74-82
 #include <stdio.h>
 #include "symbol.h"
 #include "errormsg.h"
 #include "types.h"
 
-
-static struct Ty_ty_ tynil = { Ty_nil };
-Ty_ty Ty_Nil(void) { return &tynil; }
-
+static struct Ty_ty_ tyint = { Ty_int };
 static struct Ty_ty_ tyint = { Ty_int };
 Ty_ty Ty_Int(void) { return &tyint; }
+Ty_ty Ty_Float(void) { return &tyFloat; }
+Ty_ty Ty_Char(void) { return &tyChar; }
 
-static struct Ty_ty_ tystring = { Ty_string };
-Ty_ty Ty_String(void) { return &tystring; }
 
-static struct Ty_ty_ tyvoid = { Ty_void };
-Ty_ty Ty_Void(void) { return &tyvoid; }
-
-Ty_ty Ty_Record(Ty_fieldList fields)
+Ty_ty Ty_Struc(Ty_fieldList fields)
 {
 	Ty_ty p = checked_malloc(sizeof(*p));
-	p->kind = Ty_record;
-	p->u.record = fields;
+	p->kind = Ty_struc;
+	p->u.struc = fields;
 	return p;
 }
 
@@ -36,15 +27,6 @@ Ty_ty Ty_Array(Ty_ty ty)
 	Ty_ty p = checked_malloc(sizeof(*p));
 	p->kind = Ty_array;
 	p->u.array = ty;
-	return p;
-}
-
-Ty_ty Ty_Name(S_symbol sym, Ty_ty ty)
-{
-	Ty_ty p = checked_malloc(sizeof(*p));
-	p->kind = Ty_name;
-	p->u.name.sym = sym;
-	p->u.name.ty = ty;
 	return p;
 }
 
@@ -72,31 +54,3 @@ Ty_fieldList Ty_FieldList(Ty_field head, Ty_fieldList tail)
 	return p;
 }
 
-/* printing functions - used for debugging */
-static char str_ty[][12] = {
-	"ty_record", "ty_nil", "ty_int", "ty_string",
-	"ty_array", "ty_name", "ty_void" };
-
-/* This will infinite loop on mutually recursive types */
-void Ty_print(Ty_ty t)
-{
-	if (t == NULL) printf("null");
-	else {
-		printf("%s", str_ty[t->kind]);
-		if (t->kind == Ty_name) {
-			printf(", %s", S_name(t->u.name.sym));
-		}
-	}
-}
-
-void TyList_print(Ty_tyList list)
-{
-	if (list == NULL) printf("null");
-	else {
-		printf("TyList( ");
-		Ty_print(list->head);
-		printf(", ");
-		TyList_print(list->tail);
-		printf(")");
-	}
-}
--- C--/C--/types.h
+++ C--/C--/types.h
@@ -1,39 +1,27 @@
-/*
- * types.h - 
- *
- * All types and functions declared in this header file begin with "Ty_"
- * Linked list types end with "..list"
- */
-
+//
+//
+//by LBQ
+//Reference: Textbook Page 74-82
 typedef struct Ty_ty_ *Ty_ty;
 typedef struct Ty_tyList_ *Ty_tyList;
-typedef struct Ty_field_ *Ty_field;
 typedef struct Ty_fieldList_ *Ty_fieldList;
-
-struct Ty_ty_ {enum {Ty_record, Ty_nil, Ty_int, Ty_string, Ty_array,
-		       Ty_name, Ty_void} kind;
-	       union {Ty_fieldList record;
+typedef struct Ty_field_ *Ty_field;
+struct Ty_ty_ {enum {Ty_int, Ty_float, Ty_char,Ty_array,Ty_struc
+		        } kind;  //types
+	       union {Ty_fieldList struc;  //unique types
 		      Ty_ty array;
-		      struct {S_symbol sym; Ty_ty ty;} name;
 		    } u;
 	     };
 
 struct Ty_tyList_ {Ty_ty head; Ty_tyList tail;};
-struct Ty_field_ {S_symbol name; Ty_ty ty;};
 struct Ty_fieldList_ {Ty_field head; Ty_fieldList tail;};
-
-Ty_ty Ty_Nil(void);
+struct Ty_field_ {S_symbol name; Ty_ty ty;};
 Ty_ty Ty_Int(void);
-Ty_ty Ty_String(void);
-Ty_ty Ty_Void(void);
+Ty_ty Ty_Float(void);
+Ty_ty Ty_Char(void);
 
-Ty_ty Ty_Record(Ty_fieldList fields);
-Ty_ty Ty_Array(Ty_ty ty);
-Ty_ty Ty_Name(S_symbol sym, Ty_ty ty);
+Ty_ty Ty_Struc(Ty_fieldList fields); //type struct
+Ty_ty Ty_Array(Ty_ty ty); //type array
 
 Ty_tyList Ty_TyList(Ty_ty head, Ty_tyList tail);
-Ty_field Ty_Field(S_symbol name, Ty_ty ty);
 Ty_fieldList Ty_FieldList(Ty_field head, Ty_fieldList tail);
-
-void Ty_print(Ty_ty t);
-void TyList_print(Ty_tyList list);
